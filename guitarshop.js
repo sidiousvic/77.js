9bc82c02be38b05eb3305769ef08f688c715ff37
sidiousvic <victor6rene@gmail.com>
2019-03-05 21:00:47 +0900

    create gitignore and guitarshop.js

---
+++ guitarshop.js
@@ -0,0 +1,15 @@
+const TAX_RATE = 0.09;
+const TAX_lol = 0.098;
+const GUITAR_PRICE = 999;
+const AMP_PRICE = 499;
+const SPENDING_THRESHOLD = 3000;
+
+let bankBalance = 5000;
+
+const calcTax = amount => {
+  return amount + amount * TAX_RATE;
+};
+
+const inDollars = amount => {};
+
+console.log(calcTax(GUITAR_PRICE));
