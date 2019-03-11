d815f37335a09f4fe830cee15de27aa9feda5cab
sidiousvic <victor6rene@gmail.com>
2019-03-11 21:37:01 +0900

    Update JOURNAL.md

--- exercises/sumoddfibs.js
+++ exercises/sumoddfibs.js
@@ -11,11 +11,10 @@ function sumFibs(num) {
 console.log(sumFibs(1000));
 
 // Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
-// For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
+// For example, sumFibs(10) => 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
 
-// sumFibs(1) should return a number.
-// sumFibs(1000) should return 1785.
-// sumFibs(4000000) should return 4613732.
-// sumFibs(4) should return 5.
-// sumFibs(75024) should return 60696.
-// sumFibs(75025) should return 135721.
+// sumFibs(1000) => 1785
+// sumFibs(4000000) => 4613732
+// sumFibs(4) => 5
+// sumFibs(75024) => 60696
+// sumFibs(75025) => 135721
