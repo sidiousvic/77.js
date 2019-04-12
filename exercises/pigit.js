dc39a910d7e92e7be979c18940be1255915cb50f
sidiousvic <victor6rene@gmail.com>
2019-04-12 21:44:09 +0900

    Create pigit.js

---
+++ exercises/pigit.js
@@ -0,0 +1,11 @@
+function pigIt(str) {
+  // capturing groups to replace
+  // $1. (\w) a word character
+  // $2. (\w*) a word character (greedy)
+  // $3. (\s|$) a space or end of a line
+  // replacement
+  // $2 + $1 + "ay" + $3, or "$2$1ay$3"
+  return str.replace(/(\w)(\w*)(\s|$)/g, "$2$1ay$3");
+}
+
+console.log(pigIt("Pig latin is cool"));
