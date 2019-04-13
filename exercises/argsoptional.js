df715f3cea8e79f7f53cd59854b4c2bdb3460a16
sidiousvic <victor6rene@gmail.com>
2019-04-13 19:39:58 +0900

    Update argsoptional.js

--- exercises/argsoptional.js
+++ exercises/argsoptional.js
@@ -1,4 +1,5 @@
 const addTogether = (x, y) => {
+  // build a function to test if argument is number
   let isNum = num => {
     if (typeof num != "number") {
       return undefined;
@@ -7,11 +8,14 @@ const addTogether = (x, y) => {
     }
   };
 
+  // if arguments x and y are passed and they are numbers, return their sum
   if (x && y) {
     if (isNum(x) === undefined || isNum(y) === undefined) {
       return undefined;
     }
     return x + y;
+    // if only x argument is passed, return a function that expects an argument z
+    // and returns the sum of both x and z if they are both numbers
   } else {
     if (isNum(x) === undefined) {
       return undefined;
@@ -26,4 +30,6 @@ const addTogether = (x, y) => {
 };
 
 console.log(addTogether(2, 3));
+// 5
 console.log(addTogether(2)(3));
+// 5
