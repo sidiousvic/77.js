e1ff7fb8beae6e6d3582cc02cfbabdde943c961e
sidiousvic <victor6rene@gmail.com>
2019-04-22 21:12:21 +0900

    Create maxsequence.js

---
+++ exercises/maxsequence.js
@@ -0,0 +1,13 @@
+const maxSequence = arr => {
+  let sum = 0;
+  let max = 0;
+  for (let num in arr) {
+    // let sum be the sum of tem and the current number if it is above 0.
+    sum = Math.max(0, sum + arr[num]);
+    // let max be as high as the highest tem we have processed.
+    max = Math.max(sum, max);
+  }
+  return max;
+};
+
+console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // 6
